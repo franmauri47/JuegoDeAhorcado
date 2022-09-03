eadd71ec798c83964d8f89669b0933014f2c7ed8
Francisco <franciscomauri47@gmail.com>
2022-09-02 23:07:16 -0300

    Archivo js añadido junto a lógica de seleccionar palabra aleatoria

---
+++ juego.js
@@ -0,0 +1,11 @@
+let palabras = ["MESA","AUTO","ALURA","ROMA","RATON","CALABAZA"]
+
+function sortea(n) {
+    return Math.round(Math.random() * n);
+}
+
+function palabraAleatoria () {
+    return palabras[sortea(palabras.length)];
+}
+
+/* alert(palabraAleatoria()); */
